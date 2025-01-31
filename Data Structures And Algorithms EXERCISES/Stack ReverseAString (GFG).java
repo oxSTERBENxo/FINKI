8bcea4c55c9d70ea2ea4e7922b5dba17e0cf59ac
STERBEN <eva.knezevik@gmail.com>
2025-02-01 00:34:20 +0100

    Update Stack ReverseAString (GFG).java

--- Data Structures And Algorithms EXERCISES/Stack ReverseAString (GFG).java
+++ Data Structures And Algorithms EXERCISES/Stack ReverseAString (GFG).java
@@ -1,24 +1,21 @@
 /*Problem: Given a string, reverse it using a stack.
 Concept Used: LIFO (Last In, First Out) property of the stack.*/
-
 import java.util.Scanner;
 import java.util.Stack;
 
 public class StackReverseAStringUsingStack {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         String str = sc.nextLine();
-        Stack<String> stack = new Stack<>();
+        Stack<Character> stack = new Stack<>();
 
-        String[] chars = str.split("");
-
-        for (String ch : chars) {
+        for (Character ch : str.toCharArray()){
             stack.push(ch);
         }
 
         while (!stack.isEmpty()) {
-            System.out.print(stack.pop()); 
+            System.out.print(stack.pop());
         }
     }
 }
