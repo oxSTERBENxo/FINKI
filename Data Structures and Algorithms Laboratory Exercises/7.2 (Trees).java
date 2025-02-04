3c37488c1b299de33b7d15456d80eb165c8080a1
STERBEN <eva.knezevik@gmail.com>
2025-02-04 21:29:30 +0100

    Create 7.2 (Trees).java

---
+++ Data Structures and Algorithms Laboratory Exercises/7.2 (Trees).java
@@ -0,0 +1,395 @@
+/*Во следната задача треба да изградите бинарно дрво со N јазли, за кое ќе треба да одговорите на Q прашања од видот „колку внатрешни јазли има во поддрвото на избран јазол“.
+Секој јазол ќе има уникатно име. Името на коренот на дрвото секогаш ќе ви биде дадено прво. 
+Влезот ќе содржи N+Q редови од видот
+root ime - Треба да го поставите коренот на дрвото да биде јазелот со име ime
+add parent_name child_name - Треба да додадете дете јазел со име child_name на јазелот со име parent_name
+ask node_name - Треба да го одговориме прашањето за поддрвото на јазелот со име node_name
+*/
+//input
+/*11 9
+root bravo
+add bravo echo LEFT
+add echo beard LEFT
+ask beard
+ask bravo
+add bravo foxtrot RIGHT
+add beard hotel LEFT
+add beard india RIGHT
+ask echo
+add foxtrot golf LEFT
+add golf juliet RIGHT
+add india sierra RIGHT
+ask foxtrot
+ask bravo
+ask beard
+add echo mike RIGHT
+add foxtrot tango RIGHT
+ask echo
+ask bravo
+ask foxtrot*/
+//output
+/*0
+2
+2
+2
+6
+2
+3
+6
+2*/
+
+import java.util.*;
+
+public class Trees2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int q =sc.nextInt();
+        sc.nextLine();
+
+        BTree<String> tree = new BTree<>();
+
+        for(int i = 0; i < n+q; i++){
+            String[] input = sc.nextLine().trim().split("\\s+");
+
+            if(input[0].equals("root")){
+                tree.makeRoot(input[1]);
+            }else if(input[0].equals("add")){
+                BNode<String> kade = find(tree,tree.root,input[1]);
+
+                if(input[3].equals("LEFT")){
+                    tree.addChild(kade,1,input[2]);
+                }else{
+                    tree.addChild(kade,2,input[2]);
+                }
+            }else{
+                BNode<String> kade = find(tree,tree.root,input[1]);
+                System.out.println(tree.insideNodesR(kade));
+            }
+        }
+    }
+
+    static BNode<String> find(BTree<String> tree,BNode<String>  curr, String kluc){
+        if(curr == null){
+            return null;
+        }
+        if(curr.info.equals(kluc)){
+            return curr;
+        }else{
+            BNode<String> levo = find(tree, curr.left, kluc);
+            if(levo != null){
+                return levo;
+            }
+            BNode<String> desno = find(tree, curr.right, kluc);
+            if(desno != null){
+                return desno;
+            }
+        }
+        return null;
+    }
+}
+
+//od ovde nadolu e nivniot kod
+
+interface Tree<E> {
+    // //////////Accessors ////////////
+
+    public Tree.Node<E> root();
+
+    public Tree.Node<E> parent(Tree.Node<E> node);
+
+    public int childCount(Tree.Node<E> node);
+
+    // //////////Transformers ////////////
+    public void makeRoot(E elem);
+
+    public Tree.Node<E> addChild(Tree.Node<E> node, E elem);
+
+    public void remove(Tree.Node<E> node);
+
+    // //////////Iterators ////////////
+    public Iterator<E> children(Tree.Node<E> node);
+
+    public Iterable<Tree.Node<E>> childrenNodes(Tree.Node<E> node);
+
+    // //////Inner interface for tree nodes ////////
+    public interface Node<E> {
+
+        public E getElement();
+
+        public void setElement(E elem);
+
+    }
+}
+interface Stack<E> {
+
+    // Elementi na stekot se objekti od proizvolen tip.
+
+    // Metodi za pristap:
+
+    public boolean isEmpty ();
+    // Vrakja true ako i samo ako stekot e prazen.
+
+    public E peek ();
+    // Go vrakja elementot na vrvot od stekot.
+
+    // Metodi za transformacija:
+
+    public void clear ();
+    // Go prazni stekot.
+
+    public void push (E x);
+    // Go dodava x na vrvot na stekot.
+
+    public E pop ();
+    // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+}
+class ArrayStack<E> implements Stack<E> {
+    private E[] elems;
+    private int depth;
+
+    @SuppressWarnings("unchecked")
+    public ArrayStack (int maxDepth) {
+        // Konstrukcija na nov, prazen stek.
+        elems = (E[]) new Object[maxDepth];
+        depth = 0;
+    }
+
+
+    public boolean isEmpty () {
+        // Vrakja true ako i samo ako stekot e prazen.
+        return (depth == 0);
+    }
+
+
+    public E peek () {
+        // Go vrakja elementot na vrvot od stekot.
+        if (depth == 0)
+            throw new NoSuchElementException();
+        return elems[depth-1];
+    }
+
+
+    public void clear () {
+        // Go prazni stekot.
+        for (int i = 0; i < depth; i++)  elems[i] = null;
+        depth = 0;
+    }
+
+
+    public void push (E x) {
+        // Go dodava x na vrvot na stekot.
+        elems[depth++] = x;
+    }
+
+
+    public E pop () {
+        // Go otstranuva i vrakja elementot shto e na vrvot na stekot.
+        if (depth == 0)
+            throw new NoSuchElementException();
+        E topmost = elems[--depth];
+        elems[depth] = null;
+        return topmost;
+    }
+}
+
+class BNode<E> {
+
+    public E info;
+    public BNode<E> left;
+    public BNode<E> right;
+
+    public static int LEFT = 1;
+    public static int RIGHT = 2;
+
+    public BNode<E> parent;
+    public BNode(E info, BNode<E> parent) {
+        this.parent = parent;
+        this.info = info;
+        left = null;
+        right = null;
+    }
+    public BNode(E info) {
+        this.parent = null;
+        this.info = info;
+        left = null;
+        right = null;
+    }
+
+    public BNode(E info, BNode<E> left, BNode<E> right) {
+        this.info = info;
+        this.left = left;
+        this.right = right;
+    }
+
+}
+
+class BTree<E> {
+
+    public BNode<E> root;
+
+    public BTree() {
+        root = null;
+    }
+
+    public BTree(E info) {
+        root = new BNode<E>(info);
+    }
+
+    public void makeRoot(E elem) {
+        root = new BNode<E>(elem);
+    }
+
+    public BNode<E> addChild(BNode<E> node, int where, E elem) {
+
+        BNode<E> tmp = new BNode<E>(elem, node);
+
+        if (where == BNode.LEFT) {
+            if (node.left != null)  // veke postoi element
+                return null;
+            node.left = tmp;
+        } else {
+            if (node.right != null) // veke postoi element
+                return null;
+            node.right = tmp;
+        }
+
+        return tmp;
+    }
+
+    public void inorder() {
+        System.out.print("INORDER: ");
+        inorderR(root);
+        System.out.println();
+    }
+
+    public void inorderR(BNode<E> n) {
+        if (n != null) {
+            inorderR(n.left);
+            System.out.print(n.info.toString()+" ");
+            inorderR(n.right);
+        }
+    }
+
+    public void preorder() {
+        System.out.print("PREORDER: ");
+        preorderR(root);
+        System.out.println();
+    }
+
+    public void preorderR(BNode<E> n) {
+        if (n != null) {
+            System.out.print(n.info.toString()+" ");
+            preorderR(n.left);
+            preorderR(n.right);
+        }
+    }
+
+    public void postorder() {
+        System.out.print("POSTORDER: ");
+        postorderR(root);
+        System.out.println();
+    }
+
+    public void postorderR(BNode<E> n) {
+        if (n != null) {
+            postorderR(n.left);
+            postorderR(n.right);
+            System.out.print(n.info.toString()+" ");
+        }
+    }
+
+    public void inorderNonRecursive() {
+        ArrayStack<BNode<E>> s = new ArrayStack<BNode<E>>(100);
+        BNode<E> p = root;
+        System.out.print("INORDER (nonrecursive): ");
+
+        while (true) {
+            // pridvizuvanje do kraj vo leva nasoka pri sto site koreni
+            // na potstebla se dodavaat vo magacin za podocnezna obrabotka
+            while (p != null) {
+                s.push(p);
+                p = p.left;
+            }
+
+            // ako magacinot e prazen znaci deka stebloto e celosno izminato
+            if (s.isEmpty())
+                break;
+
+            p = s.peek();
+            // pecatenje (obrabotka) na jazelot na vrvot od magacinot
+            System.out.print(p.info.toString()+" ");
+            // brisenje na obraboteniot jazel od magacinot
+            s.pop();
+            // pridvizuvanje vo desno od obraboteniot jazel i povtoruvanje na
+            // postapkata za desnoto potsteblo na jazelot
+            p = p.right;
+
+        }
+        System.out.println();
+
+    }
+
+    int insideNodesR(BNode<E> node) {
+        if (node == null)
+            return 0;
+
+        if ((node.left == null)&&(node.right == null))
+            return 0;
+
+        return insideNodesR(node.left) + insideNodesR(node.right) + 1;
+    }
+
+    public int insideNodes() {
+        return insideNodesR(root);
+    }
+
+    int leavesR(BNode<E> node) {
+        if (node != null) {
+            if ((node.left == null)&&(node.right == null))
+                return 1;
+            else
+                return (leavesR(node.left) + leavesR(node.right));
+        } else {
+            return 0;
+        }
+    }
+
+    public int leaves() {
+        return leavesR(root);
+    }
+
+    int depthR(BNode<E> node) {
+        if (node == null)
+            return 0;
+        if ((node.left == null)&&(node.right == null))
+            return 0;
+        return (1 + Math.max(depthR(node.left), depthR(node.right)));
+    }
+
+    public int depth() {
+        return depthR(root);
+    }
+
+    void mirrorR(BNode<E> node) {
+        BNode<E> tmp;
+
+        if (node == null)
+            return;
+
+        // simetricno preslikuvanje na levoto i desnoto potsteblo
+        mirrorR(node.left);
+        mirrorR(node.right);
+
+        // smena na ulogite na pokazuvacite na momentalniot jazel
+        tmp = node.left;
+        node.left = node.right;
+        node.right = tmp;
+
+    }
+
+    public void mirror() {
+        mirrorR(root);
+    }
+
+}
